2a507f7f92b218396a62f10b086cd1a065ef08a2
Sergio <shark2150388@gmail.com>
2024-05-01 13:37:45 +0200

    Imagen nueva

--- DWES/Clase/PHP/Clase-6-Paginacion/db.sql
+++ DWES/Clase/PHP/Clase-6-Paginacion/db.sql
@@ -10,3 +10,15 @@ CREATE TABLE acciones (
 );
 
 INSERT INTO acciones (fecha, lugar, nombre, descripcion, foto) VALUES ('2024-04-22','Madrid','Pedro','Planto una flor', '/subidas/flores.jpg');
+
+INSERT INTO acciones (fecha, lugar, nombre, descripcion, foto) VALUES ('2024-04-23','Toledo','Carmen','Planto un arbol', '/subidas/flores.jpg');
+
+INSERT INTO acciones (fecha, lugar, nombre, descripcion, foto) VALUES ('2024-04-24','Zaragoza','Jorge','Planto moras', '/subidas/flores.jpg');
+
+INSERT INTO acciones (fecha, lugar, nombre, descripcion, foto) VALUES ('2024-04-25','Guadalajara','Piero','Planto geranios', '/subidas/flores.jpg');
+
+INSERT INTO acciones (fecha, lugar, nombre, descripcion, foto) VALUES ('2024-04-26','Valencia','Sergio','Planto rosas', '/subidas/flores.jpg');
+
+INSERT INTO acciones (fecha, lugar, nombre, descripcion, foto) VALUES ('2024-04-27','Barcelona','Sandra','Planto un cipres', '/subidas/flores.jpg');
+
+INSERT INTO acciones (fecha, lugar, nombre, descripcion, foto) VALUES ('2024-04-28','Oviedo','Lucia','Planto una secuoya', '/subidas/flores.jpg');
