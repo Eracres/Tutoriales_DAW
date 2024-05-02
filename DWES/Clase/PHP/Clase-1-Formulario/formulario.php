f1c6180c953d23bf5da1fdc90417616e86957be6
Eracres <122403544+Eracres@users.noreply.github.com>
2024-05-02 19:26:46 +0200

    Update formulario.php

--- DWES/Clase/PHP/Clase-1-Formulario/formulario.php
+++ DWES/Clase/PHP/Clase-1-Formulario/formulario.php
@@ -2,40 +2,38 @@
 
 $errores = [];
 $hoy = date("Y");
+$hoy_int = intval($hoy);
 $page_min = 3;
 
+$titulo = isset($_POST["titulo"]) ? $_POST["titulo"] : "";
+$autor = isset($_POST["autor"]) ? $_POST["autor"] : "";
+$ano = isset($_POST["ano"]) ? $_POST["ano"] : "";
+$page = isset($_POST["page"]) ? $_POST["page"] : "";
+
 if (isset($_POST["enviar"])) {
-    if (isset($_POST["titulo"]) && $_POST["titulo"] != "") {
-        $titulo = $_POST["titulo"];
-    } else {
+    if ($titulo == "") {
         $errores["titulo"] = "Este campo es obligatorio";
     }
 
-    if (isset($_POST["autor"]) && $_POST["autor"] != "") {
-        $titulo = $_POST["autor"];
-    } else {
+    if ($autor == "") {
         $errores["autor"] = "Este campo es obligatorio";
     }
 
-    if (isset($_POST["ano"]) && $_POST["titulo"] != "") {
-        $titulo = $_POST["ano"];
-    } else if (isset($_POST["ano"]) && $_POST["ano"] < $hoy){
-        $errores["ano"] = "El año introducido no es correcto";
-    } else {
+    if ($ano == "") {
         $errores["ano"] = "Este campo es obligatorio";
+    } else if ($ano > $hoy_int) {
+        $errores["ano"] = "El año introducido no es correcto";
     }
 
-    if (isset($_POST["page"]) && $_POST["page"] != "") {
-        $titulo = $_POST["page"];
-    } else if (isset($_POST["page"]) && $_POST["page"] < $hoy){
-        $errores["page"] = "El numero de paginas es muy bajo";
-    } else {
+    if ($page == "") {
         $errores["page"] = "Este campo es obligatorio";
+    } else if ($page < $page_min) {
+        $errores["page"] = "El numero de paginas es muy bajo";
     }
 
     if (empty($errores)) {
         header("Location: exito.php");
-        die(); // o exit
+        die();
     }
 }
 
@@ -61,32 +59,28 @@ if (isset($_POST["enviar"])) {
 <body>
     <h1>Alta de libros</h1>
     <form action="formulario.php" method="post">
-        <label for="titulo">Titulo:</label><input type="text" name="titulo" id="titulo" value="<?= $titulo?>"><br>
+        <label for="titulo">Titulo:</label><input type="text" name="titulo" id="titulo" value="<?= $titulo ?>"><br>
         <?php
-            if($errores["titulo"]){
+            if(isset($errores["titulo"])){
                 echo "<span class='error'>" . $errores["titulo"] . "</span>";
-
             }
         ?>
-        <label for="autor">Autor:</label><input type="text" name="autor" id="autor" value="<?=$autor?>"><br>
+        <label for="autor">Autor:</label><input type="text" name="autor" id="autor" value="<?= $autor ?>"><br>
         <?php
-            if($errores["titulo"]){
+            if(isset($errores["autor"])){
                 echo "<span class='error'>" . $errores["autor"] . "</span>";
-
             }
         ?>
-        <label for="ano">Año:</label><input type="text" name="ano" id="ano" value="<?=$ano?>"><br>
+        <label for="ano">Año:</label><input type="text" name="ano" id="ano" value="<?= $ano ?>"><br>
         <?php
-            if($errores["titulo"]){
+            if(isset($errores["ano"])){
                 echo "<span class='error'>" . $errores["ano"] . "</span>";
-
             }
         ?>
-        <label for="page">Paginas:</label><input type="text" name="page" id="page" value="<?=$page?>"><br>
+        <label for="page">Paginas:</label><input type="text" name="page" id="page" value="<?= $page ?>"><br>
         <?php
-            if($errores["titulo"]){
+            if(isset($errores["page"])){
                 echo "<span class='error'>" . $errores["page"] . "</span>";
-
             }
         ?>
         <input type="submit" name="enviar" value="Enviar">
