39243cdf892c1f1d8938df34715667c9777ee491
Eracres <122403544+Eracres@users.noreply.github.com>
2024-05-07 18:15:49 +0200

    Update forms.py

--- DWES/Clase/Django/Clase-5-Musica/musica/canciones/forms.py
+++ DWES/Clase/Django/Clase-5-Musica/musica/canciones/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 from .models import Cancion
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+def validate_mp3(value):
+    if not value.name.endswith('.mp3'):
+        raise ValidationError(
+            _('El archivo debe ser de tipo MP3.'),
+            params={'value': value},
+        )
 
 class CancionForm(forms.ModelForm):
+    fichero = forms.FileField(
+        label='Fichero',
+        validators=[validate_mp3],
+        widget=forms.FileInput(attrs={'accept': 'audio/mpeg'}),
+    )
+
     class Meta:
         model = Cancion
         fields = ['nombre', 'artista', 'genero', 'fichero']
