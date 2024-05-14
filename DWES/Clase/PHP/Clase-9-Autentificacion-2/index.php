754675ca3452c6df2d98f7bbbf7d0abf02767364
Sergio <shark2150388@gmail.com>
2024-05-14 14:22:58 +0200

    Repetir Autentificacion

---
+++ DWES/Clase/PHP/Clase-9-Autentificacion-2/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('init.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenido</h1>
+    <p>
+        Esto es el inicio de un aprobado en DWES ... <a href="privada.php">adelante</a>
+    </p>
+</body>
+</html>
